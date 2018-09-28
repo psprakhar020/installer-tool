82ed9990595be67b6a3f34ee1903738b24624e1a
Manish Kumar <psprakhar020@gmail.com>
2018-09-28 22:09:45 +0530

    test2

--- installer.ps1
+++ installer.ps1
@@ -1,4 +1,6 @@
-﻿<Window x:Name="tool" x:Class="installer.MainWindow"
+﻿ #ERASE ALL THIS AND PUT XAML BELOW between the @" "@ 
+$inputXML = @"
+<Window x:Name="tool" x:Class="installer.MainWindow"
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
         xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
@@ -40,3 +42,67 @@
         <TextBlock HorizontalAlignment="Left" Height="21" Margin="7,107,0,0" TextWrapping="Wrap" Text="Server Name:" VerticalAlignment="Top" Width="109" FontSize="16"/>
     </Grid>
 </Window>
+
+
+
+
+"@ 
+
+$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
+[void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[xml]$XAML = $inputXML
+
+#Check for a text changed value (which we cannot parse)
+If ($xaml.SelectNodes("//*[@Name]") | ? TextChanged){write-error "This Snippet can't convert any lines which contain a 'textChanged' property. `n please manually remove these entries"
+        $xaml.SelectNodes("//*[@Name]") | ? TextChanged | % {write-warning "Please remove the TextChanged property from this entry $($_.Name)"}
+return}
+
+#Read XAML
+
+    $reader=(New-Object System.Xml.XmlNodeReader $xaml) 
+  try{$Form=[Windows.Markup.XamlReader]::Load( $reader )}
+catch [System.Management.Automation.MethodInvocationException] {
+    Write-Warning "We ran into a problem with the XAML code.  Check the syntax for this control..."
+    write-host $error[0].Exception.Message -ForegroundColor Red
+    if ($error[0].Exception.Message -like "*button*"){
+        write-warning "Ensure your &lt;button in the `$inputXML does NOT have a Click=ButtonClick property.  PS can't handle this`n`n`n`n"}
+}
+catch{#if it broke some other way :D
+    Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax and ensure .net is installed."
+        }
+
+#===========================================================================
+# Store Form Objects In PowerShell
+#===========================================================================
+
+$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $Form.FindName($_.Name)}
+
+Function Get-FormVariables{
+if ($global:ReadmeDisplay -ne $true){Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true}
+write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
+get-variable WPF*
+}
+
+Get-FormVariables
+
+#===========================================================================
+# Use this space to add code to the various form elements in your GUI
+#===========================================================================
+                                                                   
+    
+#Reference 
+
+#Adding items to a dropdown/combo box
+    #$vmpicklistView.items.Add([pscustomobject]@{'VMName'=($_).Name;Status=$_.Status;Other="Yes"})
+    
+#Setting the text of a text box to the current PC name    
+    #$WPFtextBox.Text = $env:COMPUTERNAME
+    
+#Adding code to a button, so that when clicked, it pings a system
+# $WPFbutton.Add_Click({ Test-connection -count 1 -ComputerName $WPFtextBox.Text
+# })
+#===========================================================================
+# Shows the form
+#===========================================================================
+write-host "To show the form, run the following" -ForegroundColor Cyan
+'$Form.ShowDialog() | out-null'
